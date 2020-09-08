74b444f2c87cc95ca105595114bb46c9472cd07b
lodi palle <lodipalle@gmail.com>
2020-09-08 15:16:10 +1000

    booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_042549) do
+ActiveRecord::Schema.define(version: 2020_09_08_042550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "total_price"
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "status"
+    t.bigint "permit_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["permit_id"], name: "index_bookings_on_permit_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "permits", force: :cascade do |t|
     t.string "name"
@@ -39,5 +52,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_042549) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "permits"
+  add_foreign_key "bookings", "users"
   add_foreign_key "permits", "users"
 end
